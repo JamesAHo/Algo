5cc3ea4baf1d3092a5030b9e67414206b4020504
Jenny Tran <piggy@Jennys-MacBook-Pro.local>
2023-06-01 15:22:08 -0700

    passed isPowerOfTwo test

--- src/math/PowerOfTwo/__test__/isPowerOfTwo.test.js
+++ src/math/PowerOfTwo/__test__/isPowerOfTwo.test.js
@@ -1,3 +1,14 @@
-import isPowerOfTwo from "../isPowerOfTwo";
+import isPowerOfTwo from '../isPowerOfTwo';
 
 // begin test
+describe('sPowerOfTwo', () => {
+    it('Should calculate if the number can divide itself', () => {
+        expect(isPowerOfTwo(-1)).toBe(false);
+        expect(isPowerOfTwo(4)).toBe(true);
+        expect(isPowerOfTwo(5)).toBe(false);
+        expect(isPowerOfTwo(8)).toBe(true);
+        expect(isPowerOfTwo(64)).toBe(true);
+        expect(isPowerOfTwo(1111)).toBe(false);
+
+    }) 
+})
