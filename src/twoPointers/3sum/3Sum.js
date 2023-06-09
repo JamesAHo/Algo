fd6cbda3a55c8b722a0466d4840d2066c5d0a80e
Jenny Tran <piggy@Jennys-MacBook-Pro.local>
2023-06-08 18:37:23 -0700

    passed test

--- src/twoPointers/3sum/3Sum.js
+++ src/twoPointers/3sum/3Sum.js
@@ -19,38 +19,35 @@
     // else if sum < 0 then left++
     // else sum > 0 then right--
 
-export default function ThreeSum(nums){
+export default function threeSum(nums){
     const result = [];
-    nums.sort((a,b) => a-b);
+    nums.sort((a,b) => a - b);
     for(let i = 0; i < nums.length - 2; i++){
-        // first duplicate test case
-        if(i > 0 || (i = 0 && nums[i] === nums[i - 1])){
-            continue;
-        }
         let left = i + 1;
         let right = nums.length - 1;
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
         // two sum pattern
         while(left < right){
-            let sum = nums[i] + nums[left] + nums[right];
+            const sum = nums[i] + nums[left] + nums[right];
             if(sum === 0){
-                result.push([nums[i],nums[left],nums[right]]);
-                // second duplicate left case
+                result.push([nums[i], nums[left], nums[right]]);
+                // Skip duplicate values of nums[left]
                 while(left < right && nums[left] === nums[left + 1]){
                     left++;
                 }
+                // Skip duplicate values of nums[left]
+                left++;
                 while(left < right && nums[right] === nums[right - 1]){
                     right--;
                 }
                 // move pointers
-                left++
-                right--
+                right--;
             } else if(sum < 0){
                 left++;
-            }else{
+            } else {
                 right--;
             }
         }
     }
     return result;
-
 }
--- src/twoPointers/3sum/__test__/3Sum.test.js
+++ src/twoPointers/3sum/__test__/3Sum.test.js
@@ -1,8 +1,8 @@
-import ThreeSum from "../3Sum";
+import threeSum from "../3Sum";
 
 // begin test
-describe('ThreeSum', () => {
+describe('threeSum', () => {
     it('should return numbers that sum = 0', () => {
-        expect(ThreeSum([-1,0,1,2,-1,-4])).toStrictEqual([[[-1,-1,2],[-1,0,1]]])
+        expect(threeSum([-1,0,1,2,-1,-4])).toStrictEqual([[-1,-1,2],[-1,0,1]]);
     })
 })
