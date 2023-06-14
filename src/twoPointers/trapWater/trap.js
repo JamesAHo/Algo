96a5481387ab1d7708edd3a1b2227127821f89ae
Jenny Tran <piggy@Jennys-MacBook-Pro.local>
2023-06-14 15:08:36 -0700

    added edge case for trap water problemset

--- src/twoPointers/trapWater/trap.js
+++ src/twoPointers/trapWater/trap.js
@@ -3,6 +3,9 @@
  * @return {number}
  */
 
-export default function trap(){
+export default function trap(height){
+    // edge case if the array is <= 2 return 0
+    let n = height.length;
+    if(n.length <= 2) return 0;
     
 }
