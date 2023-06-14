74d43ae7e22d0fa9708fd1ac21a113041adb4f20
Jenny Tran <piggy@Jennys-MacBook-Pro.local>
2023-06-14 15:12:02 -0700

    added notes for finding maxLeft and maxRight

--- src/twoPointers/trapWater/trap.js
+++ src/twoPointers/trapWater/trap.js
@@ -2,10 +2,17 @@
  * @param {number[]} height
  * @return {number}
  */
+// Identify left and right
+
+
+// Identify maxLeft and max Right
+    // maxleft is positioned at 0
+    // maxRight is positioned at height - 1
 
 export default function trap(height){
     // edge case if the array is <= 2 return 0
     let n = height.length;
     if(n.length <= 2) return 0;
-    
+    // 
+
 }
