f3c4e7ed27aa8af90060f8414bed0186515b3aa0
Jenny Tran <piggy@Jennys-MacBook-Pro.local>
2023-06-14 15:43:43 -0700

    added notes

--- src/twoPointers/trapWater/trap.js
+++ src/twoPointers/trapWater/trap.js
@@ -6,8 +6,8 @@
     // if left start at 0, then the water can not be trapped thus left should start at n[1];
     // if left start at height -1, the water also can not be trapped this right should start at n[height.length - 2]
 // Identify maxLeft and maxRight
-    // maxleft is positioned at 0
-    // maxRight is positioned at height - 1
+    // maxleft is positioned at height[0]
+    // maxRight is positioned at height[n - 1]
 // Two Pointer Approach so Time Complexity should be O(N) and S.C is O(1);
     // Run a while loop from left to right
     // Inside while loop check the conditions that make water trapped
@@ -16,12 +16,14 @@
                     //if the current height[left] is > maxLeft
                         // then maxLeft = current height[left]
                     // else
+                        // update the ammount water trapped
                         // water = maxLeft - current height[left]
                     // increast left++
             // else
                     // if the current height[right] is > maxRight
                         // then maxRight = current height[right]
                     // else
+                        // update the ammount water trapped
                         // water = maxRight - current height[right]
                     // decrease right--
 
