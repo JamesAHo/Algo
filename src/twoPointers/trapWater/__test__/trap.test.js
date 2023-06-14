9168eeb07840053364db4772e2ed66536c5fbed5
Jenny Tran <piggy@Jennys-MacBook-Pro.local>
2023-06-14 15:40:46 -0700

    passed test case <= 2

--- src/twoPointers/trapWater/__test__/trap.test.js
+++ src/twoPointers/trapWater/__test__/trap.test.js
@@ -5,4 +5,9 @@ describe('trap', () => {
     it('should return 6', () => {
         expect(trap([0,1,0,2,1,0,1,3,2,1,2,1])).toStrictEqual(6);
     })
+});
+describe('trap', () => {
+    it('should return 6', () => {
+        expect(trap([0,1])).toStrictEqual(0);
+    })
 })
