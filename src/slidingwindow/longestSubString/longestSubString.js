99d942196e6230c9eb17320490d631e0921c64c3
James Anh <james.anh184@gmail.com>
2023-06-19 22:33:54 -0700

    implemented code

--- src/slidingwindow/longestSubString/longestSubString.js
+++ src/slidingwindow/longestSubString/longestSubString.js
@@ -10,5 +10,20 @@
 // otherwise add character to hash map
 // then find maxSize by using Math.max(maxSize, i - left + 1)
 export default function longestSubString(s){
+    let set = new Set();
+    let left = 0;
+    let maxSize = 0;
+    for(let i = 0; i < s.length;i++){
 
+        // run the test case
+        while(set.has(s[i])){
+            set.delete(s[left]);
+            left++
+        }
+        // otherwise add character to hash map
+        set.add[s[i]];
+        // then find maxSize
+        maxSize = Math.max(maxSize, i - left + 1)
+    }
+    return maxSize;
 }
