e4b7529895823315ea2d3cd9da5a685004f19eb2
James Anh <james.anh184@gmail.com>
2023-06-19 22:29:00 -0700

    added pseudocode

--- src/slidingwindow/longestSubString/longestSubString.js
+++ src/slidingwindow/longestSubString/longestSubString.js
@@ -1,5 +1,14 @@
 // 3. Longest Substring Without Repeating Characters
-
+// Pseudocode 
+// Using left pointer, sliding window and hash map 
+// create a new set
+// let left = 0 and maxSize = 0
+// Loop over the array
+    // run a test case for duplicate characters
+        // if characters already exist in the hash map delete the leftmost character
+        // increament the left pointer by one
+// otherwise add character to hash map
+// then find maxSize by using Math.max(maxSize, i - left + 1)
 export default function longestSubString(s){
-    
+
 }
