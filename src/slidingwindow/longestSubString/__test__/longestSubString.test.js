45187608153e2c85562e4211a2cb2753d841be2f
James Anh <james.anh184@gmail.com>
2023-06-19 22:41:38 -0700

    added ne test cases

--- src/slidingwindow/longestSubString/__test__/longestSubString.test.js
+++ src/slidingwindow/longestSubString/__test__/longestSubString.test.js
@@ -4,4 +4,26 @@ describe("longestSubString", () =>{
     it("should return the longest substring", () =>{
         expect(longestSubString("abcabcbb")).toBe(3)
     })
+});
+// test case when string is empty
+describe("longestSubString", () =>{
+    it("should return the longest substring", () =>{
+        expect(longestSubString("")).toBe(0)
+    })
+})
+// test case when string is 1
+describe("longestSubString", () =>{
+    it("should return the longest substring", () =>{
+        expect(longestSubString("1")).toBe(1)
+    })
+})
+describe("longestSubString", () =>{
+    it("should return the longest substring", () =>{
+        expect(longestSubString("pwwkew")).toBe(3)
+    })
+})
+describe("longestSubString", () =>{
+    it("should return the longest substring", () =>{
+        expect(longestSubString("aaaaaaaa")).toBe(1)
+    })
 })
