58f64be551de50653544a95c6209e0d5fc092d29
Jenny Tran <piggy@Jennys-MacBook-Pro.local>
2023-06-24 21:53:54 -0700

    implemented code

--- src/binarysearch/minimumrotatedarray/findmin.js
+++ src/binarysearch/minimumrotatedarray/findmin.js
@@ -2,7 +2,34 @@
  * @param {number[]} nums
  * @return {number}
  */
-// 
-export function findmin(nums){
 
+// example:
+    // nums = [0, 1, 2, 4, 5, 6, 7]
+
+    //        left     mid       right
+    // 5 is nums[mid] + 1
+
+// identify left and right pointers
+    // left = 0 and right = nums.length - 1
+// Run a while loop from left to right
+    // find mid = (left +right) / 2
+    // case:1
+        // if min number lies on the right half of the array
+        // left = nums[mid] + 1
+    // case: 2
+        // if min number lies on the left half of the array
+        // right = mid
+// return the min number which nums[left]
+export function findmin(nums){
+    let left = 0;
+    let right = nums.length  - 1;
+    while(left < right){
+        let mid = Math.floor((left + right) / 2);
+        if(nums[mid] >= nums[right]){
+            left = nums[mid] + 1;
+        } else{
+            right = mid;
+        }
+    }
+    return nums[left];
 }
