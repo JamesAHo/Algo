5c4f5fc4974e821c3b1f0805caafbddda9dfd164
James Anh <james.anh184@gmail.com>
2023-07-03 21:55:20 -0700

    added solution

--- src/twoPointers/longestrepeatingchar/characterReplacement.js
+++ src/twoPointers/longestrepeatingchar/characterReplacement.js
@@ -5,7 +5,31 @@
  */
 
 // Pesuedocode
-//
+// Keep track of the characters count
+// 
 export default function characterReplacement(s,k){
-	let freqCount = 26
+	let freqCount = {}
+	let left = 0;
+	let right =0;
+	let longest = 0;
+	let maxFreq = 0;
+
+	// work to the right
+	while(right < s.length){
+		// frequency count the characters
+		const rightChar = s.charCodeAt(right);
+		freqCount[rightChar] = freqCount[rightChar] + 1 || 1;
+
+		// find the maxFreq
+		maxFreq = Math.max(maxFreq,freqCount[rightChar]);
+		// work on the valid window
+		while((right - left + 1) - maxFreq) {
+			const leftChar = s.charCodeAt(left)
+			freqCount[leftChar] -= 1
+			left++;
+		}
+		longest = Math.max(longest, right - left + 1);
+		right++
+	}
+	return longest;
 }
