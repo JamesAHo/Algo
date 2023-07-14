30d21ea17e52e9d5301d1c6e75e76694386807d5
Jenny Tran <piggy@Jennys-MacBook-Pro.local>
2023-07-14 16:29:58 -0700

    added Minimum Window Substring

--- src/twoPointers/containerWithWater/maxArea.js
+++ src/twoPointers/containerWithWater/maxArea.js
@@ -2,11 +2,10 @@
 
 // Area = length * height
 // length is the distance from j - i
-// 
 export default function maxArea(height){
     let area = 0;
-    let left  = 0;
     let right = height.length  - 1;
+    let left  = 0;
     while(left < right){
         area = Math.max(area, Math.min(height[left],height[right]) *(right - left))
         if(height[left] <= height[right]){
