6f3043f60b9313546d6c86343ee05a5ecaa7c115
Jenny Tran <piggy@Jennys-MacBook-Pro.local>
2023-07-17 16:11:27 -0700

    added test case and debug

--- src/stack/polishNotation/__test__/evalRPN.test.js
+++ src/stack/polishNotation/__test__/evalRPN.test.js
@@ -6,3 +6,9 @@ describe("Evaluate Reverse Polish Notation", () =>{
         expect(evalRPN(["2","1","+","3","*"])).toStrictEqual(9)
     })
 })
+
+describe("Evaluate Reverse Polish Notation", () =>{
+    it("should return output 9", () =>{
+        expect(evalRPN(["4","13","5","/","+"])).toStrictEqual(6)
+    })
+})
--- src/stack/polishNotation/evalRPN.js
+++ src/stack/polishNotation/evalRPN.js
@@ -9,11 +9,15 @@ export default function evalRPN(tokens) {
         if(tokens[c] == "+"){
             stack.push(stack.pop() + stack.pop())
         }else if(tokens[c] == "-"){
-            stack.push(stack.pop() - stack.pop())
+            let a = stack.pop();
+            let b = stack.pop()
+            stack.push(b - a)
         }else if(tokens[c] == "*"){
             stack.push(stack.pop() * stack.pop())
         }else if(tokens[c] == "/"){
-            stack.push(parseInt(stack.pop() / stack.pop()))
+            let a = stack.pop();
+            let b = stack.pop()
+            stack.push(parseInt(b/a))
         }else{
             stack.push(parseInt(parseInt(tokens[c])))
         }
