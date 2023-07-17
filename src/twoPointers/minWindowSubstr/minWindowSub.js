313e12be5d0be9d91031a7be9617a123cfde1830
James Anh <james.anh184@gmail.com>
2023-07-17 10:17:36 -0700

    fix bug

--- src/twoPointers/minWindowSubstr/minWindowSub.js
+++ src/twoPointers/minWindowSubstr/minWindowSub.js
@@ -6,47 +6,50 @@
 
 export default function minWindow(s, t) {
     let countT = new Map();
-    let countS = new Map();
-    let right = 0;
-    let left = 0;
-    let minStart = 0;
-    let minLength = Infinity;
-    let count = 0;
+  let countS = new Map();
+  let right = 0;
+  let left = 0;
+  let minStart = 0;
+  let minLength = Infinity;
+  let count = 0;
 
-   // count character of t string
-    for(let char of t ){
-        countT[char] = countT.set(char, (countS.get(char) || 0)  + 1);
+  // count characters of t string
+  for (let char of t) {
+    countT.set(char, (countT.get(char) || 0) + 1);
+  }
+
+  // sliding window
+  while (right < s.length) {
+    let rightChar = s[right];
+    if (countT.has(rightChar)) {
+      countS.set(rightChar, (countS.get(rightChar) || 0) + 1);
+      if (countS.get(rightChar) <= countT.get(rightChar)) {
+        count++;
+      }
     }
-    // sliding window 
-    while(right < s.length){
-        let rightChar = s[right];
-        if(countT.has(rightChar)){
-            countS.set(rightChar, (countT.get(rightChar) || 0) + 1);
-            if(countS.get(rightChar) <= countT.get(rightChar)){
-                count++;
-            } 
-        }
-        // check if count  = t.length\
-        // if it ise minLength = valid window
-        // and min start = left
-        while(count === t.length){
-            const validWindow = right - left + 1;
-            if(validWindow < minLength){
-                minLength = validWindow;
-                minStart = left;
-            }
 
-            const leftChar = s[left];
-            if(countT.has(leftChar)){
-                countS.set(leftChar, countS.get(leftChar)  - 1);
-                if(countS.get(leftChar) < countT.get(leftChar)){
-                    count--;
-                }
-            }
-            left++
+    // check if count = t.length
+    // if it is, minLength = valid window
+    // and minStart = left
+    while (count === t.length) {
+      const validWindow = right - left + 1;
+      if (validWindow < minLength) {
+        minLength = validWindow;
+        minStart = left;
+      }
+
+      const leftChar = s[left];
+      if (countT.has(leftChar)) {
+        countS.set(leftChar, countS.get(leftChar) - 1);
+        if (countS.get(leftChar) < (countT.get(leftChar) || 0)) {
+          count--;
         }
-        right++;
-        if(minLength === Infinity) return ""
-}
-return s.substring(minStart, minStart  + minLength )
+      }
+      left++;
+    }
+    right++;
+  }
+
+  if (minLength === Infinity) return "";
+  return s.substring(minStart, minStart + minLength);
 }
