e0b8eb09a9524ac7aa0f4de464356bfbb501559a
James Anh <james.anh184@gmail.com>
2023-07-17 10:23:38 -0700

    added test case

--- src/twoPointers/minWindowSubstr/__test__/minWindowSub.test.js
+++ src/twoPointers/minWindowSubstr/__test__/minWindowSub.test.js
@@ -1,2 +1,9 @@
-
+import minWindow from "../minWindowSub";
 // begin test
+// begin test
+describe('if string is palindrome', () => {
+    it('Should return a boolean if string is palindrome', () => {
+        expect(minWindow("ADOBECODEBANC","ABC")).toBe("BANC")
+    })
+    
+})
