796eb7a41a660322f2f1874e99e60c2547887f83
James Anh <james.anh184@gmail.com>
2023-07-17 09:34:12 -0700

    added variables

--- src/twoPointers/minWindowSubstr/minWindowSub.js
+++ src/twoPointers/minWindowSubstr/minWindowSub.js
@@ -9,11 +9,15 @@ export default function minWindow(s, t) {
     let countS = new Map();
     let right = 0;
     let left = 0;
-    let have = 0;
-    let need= 0;
+    let minStart = 0;
+    let minLength = Infinity;
+    let count = 0;
 
-    for(let char of s ){
+   
+    for(let char of t ){
         countS[char] = countS.set(char, (countS.get(char) || 0)  + 1);
     }
-    console.log(countS)
+    while(right < s.length){
+
+    }
 }
