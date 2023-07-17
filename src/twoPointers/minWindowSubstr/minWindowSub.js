f42314a70f9ccd1da949e2446c686c228a3e26fe
James Anh <james.anh184@gmail.com>
2023-07-17 09:23:01 -0700

    init  problem set

--- src/twoPointers/minWindowSubstr/minWindowSub.js
+++ src/twoPointers/minWindowSubstr/minWindowSub.js
@@ -4,3 +4,16 @@
  * @return {string}
  */
 
+export default function minWindow(s, t) {
+    let countT = new Map();
+    let countS = new Map();
+    let right = 0;
+    let left = 0;
+    let have = 0;
+    let need= 0;
+
+    for(let char of s ){
+        countS[char] = countS.set(char, (countS.get(char) || 0)  + 1);
+    }
+    console.log(countS)
+}
