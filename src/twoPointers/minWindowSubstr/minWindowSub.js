a3cae2dd2a598c05b3edbc57c1386a3c922a3f89
James Anh <james.anh184@gmail.com>
2023-07-17 09:53:41 -0700

    completed solution

--- src/twoPointers/minWindowSubstr/minWindowSub.js
+++ src/twoPointers/minWindowSubstr/minWindowSub.js
@@ -13,11 +13,40 @@ export default function minWindow(s, t) {
     let minLength = Infinity;
     let count = 0;
 
-   
+   // count character of t string
     for(let char of t ){
-        countS[char] = countS.set(char, (countS.get(char) || 0)  + 1);
+        countT[char] = countT.set(char, (countS.get(char) || 0)  + 1);
     }
+    // sliding window 
     while(right < s.length){
+        let rightChar = s[right];
+        if(countT.has(rightChar)){
+            countS.set(rightChar, (countT.get(rightChar) || 0) + 1);
+            if(countS.get(rightChar) <= countT.get(rightChar)){
+                count++;
+            } 
+        }
+        // check if count  = t.length\
+        // if it ise minLength = valid window
+        // and min start = left
+        while(count === t.length){
+            const validWindow = right - left + 1;
+            if(validWindow < minLength){
+                minLength = validWindow;
+                minStart = left;
+            }
 
-    }
+            const leftChar = s[left];
+            if(countT.has(leftChar)){
+                countS.set(leftChar, countS.get(leftChar)  - 1);
+                if(countS.get(leftChar) < countT.get(leftChar)){
+                    count--;
+                }
+            }
+            left++
+        }
+        right++;
+        if(minLength === Infinity) return ""
+}
+return s.substring(minStart, minStart  + minLength )
 }
