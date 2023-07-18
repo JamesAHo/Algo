c5b9df6d188272abd496b0b2214fece1eb154309
James Anh <james.anh184@gmail.com>
2023-07-18 13:07:32 -0700

    added test case

--- src/binarysearch/binarySearch/__test__/search.test.js
+++ src/binarysearch/binarySearch/__test__/search.test.js
@@ -1,3 +1,8 @@
 import search from "../search";
 
 
+describe("the rotated array and search the distinct values.", () =>{
+    it("should return output 4. 4 is the index of target value", () =>{
+        expect(search([4,5,6,7,0,1,2])).toStrictEqual(4)
+    })
+});
