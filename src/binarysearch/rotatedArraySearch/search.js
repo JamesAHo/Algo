e5fe2c5f5a5af0c9bc7f65c2ad330f1db2870899
James Anh <james.anh184@gmail.com>
2023-07-18 18:24:12 -0700

    added notes

--- src/binarysearch/rotatedArraySearch/search.js
+++ src/binarysearch/rotatedArraySearch/search.js
@@ -16,7 +16,9 @@ export default function search(nums,target){
 
         if (nums[l] <= nums[mid]) {
             // Left half is sorted
-
+			// if num left <= target and target is less than nums mid
+			// right = mid - 1
+			// then look the left
             if (nums[l] <= target && target < nums[mid]) {
                 r = mid - 1;
             } else {
@@ -24,7 +26,8 @@ export default function search(nums,target){
             }
         } else {
             // Right half is sorted
-
+			// if nums mid < target and target is less than num right
+			// then look to the right 
             if (nums[mid] < target && target <= nums[r]) {
                 l = mid + 1;
             } else {
@@ -32,7 +35,7 @@ export default function search(nums,target){
             }
         }
     }
-
+	// return 1 if not found
     return -1;
 }
 
