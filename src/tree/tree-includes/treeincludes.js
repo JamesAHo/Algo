c10d9bbf0a64da8cbb6a7444193a4822b0e47553
James Anh <james.anh184@gmail.com>
2023-07-21 16:42:20 -0700

    added tree nodes

--- src/tree/tree-includes/treeincludes.js
+++ src/tree/tree-includes/treeincludes.js
@@ -5,14 +5,28 @@ class Node{
         this.right = null;
     }
 }
-
-export default function treeIncludes(root, target){
+const treeIncludes = (root, target) =>{
     const queue = [ root ];
     while(queue.length > 0) {
-        const currentNode = queue.shift();
-        console.log(currentNode.val);
+        const current = queue.shift();
+        console.log(current.val);
         
-        if(current.left) queue.push(currentNode.left);
-        if(current.right) queue.push(currentNode.left);
+        if(current.left) queue.push(current.left);
+        if(current.right) queue.push(current.right);
     }
 }
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+const f = new Node("f");
+
+a.left = b;
+a.right = c;
+b.left  = d;
+b.right = e;
+c.right = f;
+
+treeIncludes(a,"e");
