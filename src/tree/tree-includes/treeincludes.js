a9098b01758d077ceed7bdcebb2c0619026bffa0
James Anh <james.anh184@gmail.com>
2023-07-21 16:46:58 -0700

    added edge case

--- src/tree/tree-includes/treeincludes.js
+++ src/tree/tree-includes/treeincludes.js
@@ -6,14 +6,17 @@ class Node{
     }
 }
 const treeIncludes = (root, target) =>{
+    // if root is null return false
+    if(root === null) return false;
     const queue = [ root ];
     while(queue.length > 0) {
         const current = queue.shift();
         console.log(current.val);
-        
+        if(current.val === target) return true
         if(current.left) queue.push(current.left);
         if(current.right) queue.push(current.right);
     }
+    return false
 }
 
 const a = new Node("a");
