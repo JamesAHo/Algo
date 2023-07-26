3735d2635b23d74e562346400baecd788f845715
Jenny Tran <piggy@Jennys-MacBook-Pro.local>
2023-07-26 10:05:48 -0700

    added solution

--- src/twoPointers/fruitsBasket/fruitbasket.js
+++ src/twoPointers/fruitsBasket/fruitbasket.js
@@ -4,5 +4,26 @@
  */
 
 export default function totalFruit(fruits){
-    
+    let left = 0;
+    let right = 0;
+    let basket = new Map();
+    let ans = 0
+    while(right < fruits.length){
+        basket.set(fruits[right],(fruits[right] || 0) + 1);
+        
+        // valid window condition
+        while(basket.size > 2){
+            const leftFruit = fruits[left];
+            const count = basket.get(leftFruit);
+            if(count === 0){
+                basket.delete(leftFruit)
+            }else{
+                basket.set(leftFruit, count -1)
+            }
+            left++
+        }
+        right++
+        ans = Math.max(ans,right - left)
+    }
+    return ans
 }
