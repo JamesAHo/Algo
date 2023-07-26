dc0163fabf446e7e68caa2e22fe7bd3732f9e586
Jenny Tran <piggy@Jennys-MacBook-Pro.local>
2023-07-26 08:54:27 -0700

    added test case

--- src/twoPointers/fruitsBasket/__test__/fruitbasket.test.js
+++ src/twoPointers/fruitsBasket/__test__/fruitbasket.test.js
@@ -1,3 +1,9 @@
 import totalFruit from "../fruitbasket";
 
 //begin test
+describe('Given the integer array fruits, return the maximum number of fruits you can pick.', () => {
+    it('Should return 3 as output becase We can pick from all 3 trees.', () => {
+        expect(totalFruit([1,2,1])).toBe(3)
+    })
+    
+})
