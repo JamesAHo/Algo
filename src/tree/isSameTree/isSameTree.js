2459f16e6cc7833cdb2f7861072d545f87dbcfc7
Jenny Tran <piggy@Jennys-MacBook-Pro.local>
2023-07-26 15:42:33 -0700

    added sol for isSame tree set

--- src/tree/isSameTree/isSameTree.js
+++ src/tree/isSameTree/isSameTree.js
@@ -12,6 +12,19 @@
  * @return {boolean}
  */
 export default function isSametree(p,q){
-    
+    // if p and w are null, return true
+    if(!p && !q) return true
+    // if neither p or q is null return false because tree is not the same
+    if(!p || !q) return false
+    // if values from both trees are not the same, return false
+    if(p.val !== q.val) return false
+
+    // recursively check left and right of the roots
+    if(!isSametree(root.left,q.left)) return false;
+    if(!isSametree(root.right,q.right)) return false;
+    // otherwise return true
+    return true;
 }
+
+
 
