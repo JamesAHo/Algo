b1b7b32954681f8e4c557a391c0e341e947860de
Jenny Tran <piggy@Jennys-MacBook-Pro.local>
2023-08-22 20:46:38 -0700

    added  solution

--- src/graph/hasPath/hasPath.js
+++ src/graph/hasPath/hasPath.js
@@ -15,5 +15,13 @@ const graph = {
 // using DFS traversal 
 
 export default function hasPath(graph, source, dst){
+    // return true if the source = destination
+    if(source === dst) return true
+    for(let neighbor of graph[source]){
+        if(hasPath(graph, neighbor, dst) === true){
+            return true
+        }
 
+    }
+    return false
 }
