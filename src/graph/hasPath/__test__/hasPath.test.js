e36c6ed50e9d2238e80c944fc1c0d017fdaa6f3a
Jenny Tran <piggy@Jennys-MacBook-Pro.local>
2023-08-22 20:37:23 -0700

    added graph algo

---
+++ src/graph/hasPath/__test__/hasPath.test.js
@@ -0,0 +1,2 @@
+import hasPath from "../hasPath";
+
---
+++ src/graph/hasPath/hasPath.js
@@ -0,0 +1,19 @@
+
+const graph = {
+    f: [g,i],
+    g: [h],
+    h: [],
+    i: [g,k],
+    j: [j],
+    k: []
+}
+
+// n = # of nodes
+// e = # of edges
+// time complexity is O(e)
+// space complexity is O(n)
+// using DFS traversal 
+
+export default function hasPath(graph, source, graph){
+
+}
