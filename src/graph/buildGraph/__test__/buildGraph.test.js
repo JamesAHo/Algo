6178f277eecc572de9aea74b5ad61841bc5e9c0e
Jenny Tran <piggy@Jennys-MacBook-Pro.local>
2023-08-23 21:27:07 -0700

    passed test case

--- src/graph/buildGraph/__test__/buildGraph.test.js
+++ src/graph/buildGraph/__test__/buildGraph.test.js
@@ -1,3 +1,9 @@
 import buildGraph from "../buildGraph"
 
 
+const edges = [[0,1],[1,2],[2,0]]
+describe("the graph that build from the given edges",()=>{
+    it('return an object, there are nodes with associated edges',()=>{
+        expect(buildGraph(edges)).toStrictEqual({ '0': [ 1, 2 ], '1': [ 0, 2 ], '2': [ 1, 0 ] })
+    })
+})
