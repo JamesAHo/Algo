f2af6a8efa5a184959e79534f9b0dde1c9b9a28b
Jenny Tran <piggy@Jennys-MacBook-Pro.local>
2023-08-26 21:38:35 -0700

    added main function logic

--- src/graph/numberOfIsland/numberIsland.js
+++ src/graph/numberOfIsland/numberIsland.js
@@ -6,6 +6,20 @@
 // An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
 
 export default function numberIsland(grid){
+    let visited = new Set();
+    let count = 0
+    for(let r = 0; r < grid.length; r++){
+        for(let c = 0; c < grid[0].length;c++){
+            // traverse through the grid with cow col and visited nodes
+            if(traverse(grid,r,c,visited) === true){
+                count++
+            }
+        }
+    }
 
+
+    return count;
 }
+
+
 
