e7ff25d6510184d60592c4466be602d67ee9cdc0
James Anh <james.anh184@gmail.com>
2023-09-20 20:28:38 -0700

    added remove elements problem set

---
+++ src/twoPointers/removeEl/__test__/removeEl.test.js
@@ -0,0 +1,7 @@
+import removeElements from "../removeElements";
+
+describe("remove Elements", () =>{
+    it("should return the expected answer with correct length.", () =>{
+        expect(removeElements()).toBe
+    })
+})
---
+++ src/twoPointers/removeEl/removeElements.js
@@ -0,0 +1,4 @@
+// Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. 
+export default function removeElements(nums,val){
+    
+}
