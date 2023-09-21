e2b02f0c709cc92bb688eb934735797befea2fa2
James Anh <james.anh184@gmail.com>
2023-09-20 20:31:45 -0700

    implemented solution

--- src/twoPointers/removeEl/__test__/removeEl.test.js
+++ src/twoPointers/removeEl/__test__/removeEl.test.js
@@ -2,6 +2,6 @@ import removeElements from "../removeElements";
 
 describe("remove Elements", () =>{
     it("should return the expected answer with correct length.", () =>{
-        expect(removeElements()).toBe
+        expect(removeElements( [3,2,2,3], 3)).toBe(2)
     })
 })
--- src/twoPointers/removeEl/removeElements.js
+++ src/twoPointers/removeEl/removeElements.js
@@ -1,4 +1,15 @@
 // Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. 
 export default function removeElements(nums,val){
-    
+    // using two pointers approach
+    let l = 0;
+    let r = nums.length -1;
+    while(l <= r){
+        if(nums[l] === val){
+            nums[l] = nums[r];
+            right++
+        }else{
+            left++
+        }
+    }
+    return left;
 }
