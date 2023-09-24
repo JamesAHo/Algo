d59f5f3ded888e738197a3e54a575b87c8af1faf
James Anh <james.anh184@gmail.com>
2023-09-24 08:42:42 -0700

    added nums into param and unit testing

--- src/twoPointers/moveZero/__test__/moveZero.test.js
+++ src/twoPointers/moveZero/__test__/moveZero.test.js
@@ -1,1 +1,12 @@
 import moveZero from "../moveZero";
+
+
+
+// begin test
+const numbers = [0,1,0,3,12]
+const target = 9
+describe('Given an integer array nums, move all 0s  to the end of it while maintaining the relative order of the non-zero elements. ', () => {
+    it('Should push all zeros at the end of the array', () => {
+        expect(moveZero(numbers)).toBe([1,3,12,0,0])
+    })
+} )
--- src/twoPointers/moveZero/moveZero.js
+++ src/twoPointers/moveZero/moveZero.js
@@ -1,6 +1,6 @@
 // Given an integer array nums, move all zero to the end of it while maintaining the relative order of the non-zero
 
-export default function moveZero (){
+export default function moveZero(nums){
     // use fast and slow two pointers
     let left = 0;
     let right = 0;
