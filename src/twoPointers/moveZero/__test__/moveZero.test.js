144b6f93301f18a20ada9327981daedfa61dc095
James Anh <james.anh184@gmail.com>
2023-09-24 08:38:11 -0700

    added solution for removeZero problem set

---
+++ src/twoPointers/moveZero/__test__/moveZero.test.js
@@ -0,0 +1,1 @@
+import moveZero from "../moveZero";
---
+++ src/twoPointers/moveZero/moveZero.js
@@ -0,0 +1,14 @@
+// Given an integer array nums, move all zero to the end of it while maintaining the relative order of the non-zero
+
+export default function moveZero (){
+    // use fast and slow two pointers
+    let left = 0;
+    let right = 0;
+    while(right < nums.length){
+        if(nums[right] !== 0){
+            [nums[left],nums[right]] = [nums[right], nums[left]];
+            left++
+        }
+        right++
+    }
+}
