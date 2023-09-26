1066571b091a52d4b974f3b92adab183343dbd9a
James Anh <james.anh184@gmail.com>
2023-09-26 16:49:17 -0700

    added new test case for moveZero

--- src/twoPointers/moveZero/__test__/moveZero.test.js
+++ src/twoPointers/moveZero/__test__/moveZero.test.js
@@ -3,10 +3,15 @@ import moveZero from "../moveZero";
 
 
 // begin test
-const numbers = [0,1,0,3,12]
-const target = 9
+const nums = [0,1,0,3,12]
+const nums2 = [0,2,2,0,12]
 describe('Given an integer array nums, move all 0s  to the end of it while maintaining the relative order of the non-zero elements. ', () => {
     it('Should push all zeros at the end of the array', () => {
-        expect(moveZero(numbers)).toBe([1,3,12,0,0])
+        expect(moveZero(nums)).toStrictEqual([1,3,12,0,0])
+    })
+} )
+describe('Given an integer array nums, move all 0s  to the end of it while maintaining the relative order of the non-zero elements. ', () => {
+    it('Should push all zeros at the end of the array', () => {
+        expect(moveZero(nums2)).toStrictEqual([2,2,12,0,0])
     })
 } )
