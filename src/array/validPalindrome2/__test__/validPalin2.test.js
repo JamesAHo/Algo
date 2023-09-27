fcca0581ad7dcaa1278d0626d462bbe1394b2fe4
James Anh <james.anh184@gmail.com>
2023-09-27 15:19:49 -0700

    added another test case for validPalin2

--- src/array/validPalindrome2/__test__/validPalin2.test.js
+++ src/array/validPalindrome2/__test__/validPalin2.test.js
@@ -10,4 +10,9 @@ describe("validPalindrome2", () => {
     it('Given a string s, return true if the s can be palindrome after deleting at most one character from it.', () => {
         expect(validPalindrome2("abca")).toBe(true);   
     })
+});
+describe("validPalindrome2", () => {
+    it('Given a string s, return true if the s can be palindrome after deleting at most one character from it.', () => {
+        expect(validPalindrome2("abc")).toBe(false);   
+    })
 })
