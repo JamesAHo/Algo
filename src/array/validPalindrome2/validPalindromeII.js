0ceed3ed04f23055273469e298c8d44555f6f649
James Anh <james.anh184@gmail.com>
2023-09-27 15:13:14 -0700

    added solution for validPalindrome2

--- src/array/validPalindrome2/validPalindromeII.js
+++ src/array/validPalindrome2/validPalindromeII.js
@@ -5,8 +5,26 @@
  */
 
 const isValidPalindrome = (s, left, right) => {
-    
+    while(left < right){
+        if(s[left] !== s[right]){
+            return false;
+        }
+        left++;
+        right++;
+    }
+    return true;
 }
 export default function validPalindrome2(s) {
-    
+    let left = 0;
+    let right = s.length - 1;
+    while(left < right){
+        if(s[left] !== s[right]){
+            if(isValidPalindrome(s, left + 1, right)) return true;
+            if(isValidPalindrome(s, left, right - 1)) return true;
+            return false;
+        }
+        left++;
+        right++;
+    }
+    return true;
 };
