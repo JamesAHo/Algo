58ea6a6c0d72e37bb579f138d8d9237310328c57
James Anh <james.anh184@gmail.com>
2023-09-27 15:17:35 -0700

    fixes bug in solution and passed test case

---
+++ src/array/validPalindrome2/__test__/validPalin2.test.js
@@ -0,0 +1,8 @@
+import validPalindrome2 from "../validPalindromeII";
+// Begin test
+const s = "abca"
+describe("validPalindrome2", () => {
+    it('Given a string s, return true if the s can be palindrome after deleting at most one character from it.', () => {
+        expect(validPalindrome2(s)).toBe(true);   
+    })
+})
