1f1c1defb8e7f2e79397cf7904f9904e1885d7e2
James Anh <james.anh184@gmail.com>
2023-10-01 10:25:05 -0700

    added new problem set

---
+++ src/FBHackerCup/cheeseburgerCorollary/buildDecker.js
@@ -0,0 +1,9 @@
+/*This problem shares some similarities with A2, with key differences in bold.
+Problem solving skills are applicable to many daily musings. For instance, you might ponder over shared birthdays, bird houses, mapmaking, or ordering an exact number of chicken nuggets. Naturally, another great question to ponder is: how many deckers of a cheeseburger you could build if you spent your entire salary on fast food!
+Specifically, you're interested in building a 
+�
+K-decker cheeseburger, which alternates between buns, cheese, and patty starting and ending with a bun. You've already bought 
+�
+S single cheeseburgers and 
+�
+D double cheeseburgers. Each provides you with two buns, though a single provides one patty and one cheese, while a double provides two patties and two cheese. */
