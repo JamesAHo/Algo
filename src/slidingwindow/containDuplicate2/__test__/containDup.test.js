810dfb35527808398052768fb21fd6df9568aa84
James Anh <james.anh184@gmail.com>
2023-10-05 15:52:58 -0700

    added new test case for contain duplication 2

--- src/slidingwindow/containDuplicate2/__test__/containDup.test.js
+++ src/slidingwindow/containDuplicate2/__test__/containDup.test.js
@@ -1,9 +1,15 @@
 import containDup from "../containDup"
+import containDup2 from "../containDup";
 // begin test
 let nums =[1,2,3,1];
 let k = 3;
 describe("maxProfit",()=>{
     it('Should return the best time to buy and sell stock.',()=>{
         expect(containDup(nums, k)).toEqual(true);
+    })
+})
+describe("maxProfit",()=>{
+    it('Should return the best time to buy and sell stock.',()=>{
+        expect(containDup2(nums, k)).toEqual(true);
     })
 })
--- src/slidingwindow/containDuplicate2/containDup.js
+++ src/slidingwindow/containDuplicate2/containDup.js
@@ -12,3 +12,30 @@ export default function containDup(nums,k){
     }
     return false;
 }
+// using sliding window methods
+export default function containDup2(nums,k){
+    const numIndices = new Map();  // Map to store the indices of numbers
+
+    for (let left = 0, right = 0; right < nums.length; right++) {
+        const num = nums[right];
+
+        // If the current element is already in the window,
+        // and the absolute difference between indices is less than or equal to k, return true
+        if (numIndices.has(num) && right - numIndices.get(num) <= k) {
+            return true;
+        }
+
+        // Otherwise, update the index of the current element in the map
+        numIndices.set(num, right);
+
+        // If the size of the window exceeds k, move the left pointer to the right
+        if (right - left >= k) {
+            numIndices.delete(nums[left]);
+            left++;
+        }
+    }
+
+    // If no such pair of indices is found, return false
+    return false;
+
+}
