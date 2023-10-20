ad65b4a3906a6b93981f976763a979eeb390b6e8
James Anh <james.anh184@gmail.com>
2023-10-19 20:25:49 -0700

    fixes and adjustment of reorderlist problem

--- src/linkedList/reorderList/reOrderList.js
+++ src/linkedList/reorderList/reOrderList.js
@@ -16,31 +16,40 @@
 // 3: merge the two lists together 
 
 var reorderList = function(head) {
-    if(!head) return [];
-    // step 1: find the middle of the list
+    if (!head || !head.next) {
+        return head;
+    }
+
+    // Step 1: Find the middle of the list
     let slow = head;
     let fast = head;
-    while(fast.next && fast.next.next){
+    while (fast && fast.next) {
         slow = slow.next;
         fast = fast.next.next;
     }
-    // step 2: Reverse the second half of the list
+
+    // Step 2: Reverse the second half of the list
     let prev = null;
-    let curr = slow.next;
-    while(curr){
+    let curr = slow;
+    while (curr) {
         const nextt = curr.next;
         curr.next = prev;
         prev = curr;
         curr = nextt;
     }
-    slow.next = null;
-    // step 3: merge lists
+
+    // Step 3: Merge lists
     let head1 = head;
     let head2 = prev;
-    nextt = head1.next;
-    head1.next = head2;
-    head1 = head2;
-    head2 = nextt
+    
+    while (head2.next) {
+        const nextt1 = head1.next;
+        const nextt2 = head2.next;
+        head1.next = head2;
+        head1 = nextt1;
+        head2.next = head1;
+        head2 = nextt2;
+    }
 
 
 };
