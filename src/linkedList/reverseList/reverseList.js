641a231bc6b7136c84a9e5cf2f3597e842fef3b8
James Anh <james.anh184@gmail.com>
2023-10-20 19:50:03 -0700

    adjustment

--- src/linkedList/reverseList/reverseList.js
+++ src/linkedList/reverseList/reverseList.js
@@ -6,6 +6,14 @@ class Node {
 }
 
 const reverseList = (head) => {
-    
+    let prev = null;
+    let current = head;
+    while(current != null) {
+        const next = current.next;
+        current.next = prev;
+        prev = current;
+        current = next;
+    }
+    return prev;
   };
   
