ece1798258c3fb6308eecadd78c0502a39a018ba
James Anh <james.anh184@gmail.com>
2023-10-29 23:22:19 -0700

    added new test case for evalRPN

--- src/stack/evalRPN/__test__/evalRPN.test.js
+++ src/stack/evalRPN/__test__/evalRPN.test.js
@@ -2,8 +2,14 @@ import evalRPN from "../evalRPN";
 
 // begin test
 let token = ["2","1","+","3","*"];
+let token2 = ["4","13","5","/","+"]
 describe("trings tokens that represents an arithmetic expression in a Reverse Polish Notation.", () =>{
     it("Return an integer that represents the value of the expression.", () =>{
         expect(evalRPN(token)).toBe(9)
+    })
+})
+describe("trings tokens that represents an arithmetic expression in a Reverse Polish Notation.", () =>{
+    it("Return an integer that represents the value of the expression.", () =>{
+        expect(evalRPN(token2)).toBe(6)
     })
 })
