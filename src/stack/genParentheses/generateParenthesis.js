6f9ef160c6fe9ff8d65ee6b7a29cd464e0e9093e
James Anh <james.anh184@gmail.com>
2023-10-31 15:36:17 -0700

    added solution for generate parenthesis

--- src/stack/genParentheses/generateParenthesis.js
+++ src/stack/genParentheses/generateParenthesis.js
@@ -1,5 +1,19 @@
 // Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
 
 export default function generateParenthesis(n){
-    
+    const stack = [];
+    let res = []
+
+    let iterate = (str, open, close) =>{
+        if(open > n || close >n || close >open) return;
+        if(str.length == n *2 && open ==close){
+            res.push(str)
+            return;
+        }
+        iterate(str +'(',open+1,close)
+        iterate(str + ')', open, close+1)
+    }
+
+    iterate('',0,0)
+    return res;
 }
