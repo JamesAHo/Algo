f1a67fa46871bd26cb671ed2ae92f5a7a522f5a0
James Anh <james.anh184@gmail.com>
2023-10-31 15:40:31 -0700

    added and passed new test case

--- src/stack/genParentheses/__test__/generateParenthesis.test.js
+++ src/stack/genParentheses/__test__/generateParenthesis.test.js
@@ -1,8 +1,14 @@
 import generateParenthesis from "../generateParenthesis";
 // begin test
 let n = 3
+let n2 = 1
 describe("generate parenthesis", () =>{
     it("should write a function to generate all combinations of well-formed parenthesis", () =>{
         expect(generateParenthesis(n)).toStrictEqual(["((()))","(()())","(())()","()(())","()()()"])
+    })
+})
+describe("generate parenthesis", () =>{
+    it("should return 1 set of ()", () =>{
+        expect(generateParenthesis(n2)).toStrictEqual(["()"])
     })
 })
