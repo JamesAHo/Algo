74a03dec645b96715764f510782cb4c373d1a13f
James Anh <james.anh184@gmail.com>
2023-11-01 15:52:24 -0700

    adjustment

--- src/stack/genParentheses/generateParenthesis.js
+++ src/stack/genParentheses/generateParenthesis.js
@@ -4,15 +4,15 @@ export default function generateParenthesis(n){
     let res = []
 
     let iterate = (str, open, close) =>{
-        if(open > n || close >n || close >open) return;
-        if(str.length == n *2 && open ==close){
+        if(open > n || close > n || close > open) return;
+        if(str.length == n *2 && open == close){
             res.push(str)
             return;
         }
-        iterate(str +'(',open+1,close)
-        iterate(str + ')', open, close+1)
+        iterate(str +'(', open+1, close)
+        iterate(str + ')', open, close + 1)
     }
 
-    iterate('',0,0);
+    iterate('', 0, 0);
     return res;
 }
