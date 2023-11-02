b37ca225844d16b9ef3a6c59a5ccbac4236cef02
James Anh <james.anh184@gmail.com>
2023-11-02 15:32:23 -0700

    added solution

--- src/stack/dailytemperature/dailyTemperature.js
+++ src/stack/dailytemperature/dailyTemperature.js
@@ -3,5 +3,17 @@
  * @return {number[]}
  */
 export default function dailyTemperature(temperatures) {
-    
+    const n = temperatures.length;
+    const answer = new Array(n).fill(0);
+    const stack = [];
+
+    for (let i = 0; i < n; i++) {
+        while (stack.length > 0 && temperatures[i] > temperatures[stack[stack.length - 1]]) {
+            const j = stack.pop();
+            answer[j] = i - j;
+        }
+        stack.push(i);
+    }
+
+    return answer;
 }
