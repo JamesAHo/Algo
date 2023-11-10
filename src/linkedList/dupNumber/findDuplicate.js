c6c6fe0c616472adcaede5dafaf1b54d8cb73e94
James Anh <james.anh184@gmail.com>
2023-11-09 23:51:25 -0800

    added solution

--- src/linkedList/dupNumber/findDuplicate.js
+++ src/linkedList/dupNumber/findDuplicate.js
@@ -10,4 +10,22 @@ There is only one repeated number in nums, return this repeated number.
 You must solve the problem without modifying the array nums and uses only constant extra space. */
 export default function findDuplicate(nums) {
     // begin 
+    let slow = nums[0];
+    let fast = nums[0];
+
+    // Phase 1: Detect the intersection point of the two pointers
+    do {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    } while (slow !== fast);
+
+    // Phase 2: Find the entrance to the cycle
+    slow = nums[0];
+    while (slow !== fast) {
+        slow = nums[slow];
+        fast = nums[fast];
+    }
+
+    // The entrance to the cycle is the repeated element
+    return slow;
 }
