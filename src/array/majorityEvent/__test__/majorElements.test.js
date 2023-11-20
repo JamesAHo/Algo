3c5016f0dd53a75c1ef81cde6ced6ad063a871ef
James Anh <james.anh184@gmail.com>
2023-11-19 22:02:35 -0800

    added and pass test case

--- src/array/majorityEvent/__test__/majorElements.test.js
+++ src/array/majorityEvent/__test__/majorElements.test.js
@@ -1,3 +1,10 @@
 import majorityEle from "../majorElements";
 
 // begin test
+
+describe('return the majority element', () => {
+    it('should return the majority elements', () => {
+        expect(majorityEle([1,2,3,2,2,2,5,4,2])).toBe(2);
+        
+    })
+})
