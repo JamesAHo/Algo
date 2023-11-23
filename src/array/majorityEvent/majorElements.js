0e720eb4bf818aa57546261f6647e2173d5675d9
James Anh <james.anh184@gmail.com>
2023-11-22 21:27:15 -0800

    added Boyer's Moore algorithm

--- src/array/majorityEvent/majorElements.js
+++ src/array/majorityEvent/majorElements.js
@@ -2,36 +2,63 @@
  * @param {number[]} nums
  * @return {number}
  */
-export default function majorityEle(nums) {
-      // Initialize the candidate and count
-      let candidate = nums[0];
-      let count = 1;
+// export default function majorityEle(nums) {
+//       // Initialize the candidate and count
+//       let candidate = nums[0];
+//       let count = 1;
   
-      // Voting phase
-      for (let i = 1; i < nums.length; i++) {
-          if (count === 0) {
-              candidate = nums[i];
-              count = 1;
-          } else if (candidate === nums[i]) {
-              count++;
-          } else {
-              count--;
-          }
-      }
+//       // Voting phase
+//       for (let i = 1; i < nums.length; i++) {
+//           if (count === 0) {
+//               candidate = nums[i];
+//               count = 1;
+//           } else if (candidate === nums[i]) {
+//               count++;
+//           } else {
+//               count--;
+//           }
+//       }
   
-      // Verification phase
-      count = 0;
-      for (let i = 0; i < nums.length; i++) {
-          if (nums[i] === candidate) {
-              count++;
-          }
-      }
+//       // Verification phase
+//       count = 0;
+//       for (let i = 0; i < nums.length; i++) {
+//           if (nums[i] === candidate) {
+//               count++;
+//           }
+//       }
   
-      // Check if the candidate is the majority element
-      if (count > Math.floor(nums.length / 2)) {
-          return candidate;
-      } else {
-          // This case should not occur as the problem assumes a majority element always exists
-          return null;
-      }
-};
+//       // Check if the candidate is the majority element
+//       if (count > Math.floor(nums.length / 2)) {
+//           return candidate;
+//       } else {
+//           // This case should not occur as the problem assumes a majority element always exists
+//           return null;
+//       }
+// };
+
+// Using Boyer-Moore Voting Algorithm
+export default function majorityEle(nums){
+    let count = 0;
+    let candidate = null;
+    for(let i = 0; i < nums.length; i++){
+        if(count === 0){
+            candidate = nums[i];
+            count = 1;
+        } else if(candidate === nums[i]){
+            count++;
+        } else {
+            count--;
+        }
+    }
+    count = 0;
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] === candidate){
+            count++;
+        }
+    }
+    if(count > Math.floor(nums.length / 2)){
+        return candidate;
+    } else {
+        return null;
+    }
+}
