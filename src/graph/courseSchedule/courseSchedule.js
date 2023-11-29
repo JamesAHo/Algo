800568b81eed4fd21736c54cffba59f9547a8bfb
James Anh <james.anh184@gmail.com>
2023-11-28 17:12:02 -0800

    ..

--- src/graph/courseSchedule/courseSchedule.js
+++ src/graph/courseSchedule/courseSchedule.js
@@ -8,19 +8,19 @@ import buildGraph from "../buildGraph/buildGraph";
 
 export default function courseSchedule(numCourses, prerequisites){
     let visited = new Set();
-    let graph = buildGraph(prerequisites)
+    let graph = buildGraph(prerequisites);
     console.log(graph);
     for(let course = 0; course < numCourses; course++){
         if(!explore(graph,course,visited)){
             return false;
         }
     }
-    return true
+    return true;
 }
 var explore = function(graph, course, visited){
 
     if(visited.has(numCourses)) return false;
-    visited.add(numCourses)
+    visited.add(numCourses);
     for(let nei of graph[course]){
         if(!explore(graph, prerequisites, visited) === true){
             return false;
