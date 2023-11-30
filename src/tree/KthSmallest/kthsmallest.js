2bdacc9b5bfd660fad87e966baef41038c8eb7be
James Anh <james.anh184@gmail.com>
2023-11-29 20:41:44 -0800

    added kth smallest element problem

---
+++ src/tree/KthSmallest/kthsmallest.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function(root, k) {
+    let res = [];
+    if(root === null) return null;
+    function traverse(node){
+        if(node === null) return;
+        traverse(node.left);
+        result.push(node.val);
+        traverse(node.right);
+    }
+    traverse(root);
+    return res[k-1];
+};
